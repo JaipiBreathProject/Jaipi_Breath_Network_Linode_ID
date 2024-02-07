65027b97cf2f1054c274f12377209038818efc5c
JaipiBreathProject <159035641+JaipiBreathProject@users.noreply.github.com>
2024-02-07 18:33:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'fb855570-97ee-4a2e-be54-11a3bc1f56e7';
 
-const proxyIPs = ['35.219.50.99'];
+const proxyIPs = ['72.232.235.85'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
